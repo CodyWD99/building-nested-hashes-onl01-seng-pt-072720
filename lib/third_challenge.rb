0b9f9ea55e448fd1f78cc6ccbcf6903a08843944
Cody Dupuis <codydupuis99@gmail.com>
2020-08-06 11:35:06 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,32 +3,32 @@ def third_challenge
    :montague => {
       :patriarch => {
        name: "Lord Montague",
-       age: "57",
+       age: "57"
         },
       :matriarch => {
         name: "Lady Montague",
-        age: "57",
+        age: "57"
         },
       :hero => {
         name: "Romeo",
         age: "15",
-        status: "alive",
+        status: "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
         name: "Lord Capulet",
-        age: "50",
+        age: "50"
         },
       :matriarch => {
         name: "Lady Capulet",
-        age: "51",
+        age: "51"
         },
       :heroine => {
         name: "Juliet",
         age: "15",
-        status: "alive",
+        status: "alive"
         },
       :heroine_friends => []
    }
